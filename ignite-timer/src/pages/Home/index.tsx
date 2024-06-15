60a47fa167213b54f7784fecfac3ffe14b49933b
Albert Allan <albertallanramos@gmail.com>
2024-06-15 15:00:50 -0300

    Funcionalidades da aplicação - Iniciando novo ciclo

--- ignite-timer/src/pages/Home/index.tsx
+++ ignite-timer/src/pages/Home/index.tsx
@@ -1,4 +1,8 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Play } from "phosphor-react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
 import {
   CountdownContainer,
   FormContainer,
@@ -9,14 +13,71 @@ import {
   TaskInput,
 } from "./style";
 
+// interface NewCycleFormType {
+//   task: string;
+//   minutesAmount: number;
+// }
+
+const newCycleFormValidationSchema = z.object({
+  task: z.string(),
+  minutesAmountInput: z.number(),
+});
+
+// Sempre que você for referenciar uma variavel JS dentro do TypeScript, necessita o uso do typeof pois o TypeScript não consegue entender a variavel JS de forma autonoma
+type NewCycleFormType = z.infer<typeof newCycleFormValidationSchema>;
+//Fazendo isso esse type pega a tipagem de dentro do schema e fazendo assim ele dinamico de acordo que o schema é alterado
+
+interface Cycle {
+  id: string;
+  task: string;
+  minutesAmount: number;
+}
+
 export function Home() {
+  const [cycles, setCycles] = useState<Cycle[]>([]);
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm<NewCycleFormType>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      minutesAmountInput: 0,
+      task: "",
+    },
+  });
+  console.log("ERRO", errors);
+  function handleCreateNewCycle(data: NewCycleFormType) {
+    console.log("DATA", data);
+    const newCycle: Cycle = {
+      id: String(new Date().getTime()),
+      task: data.task,
+      minutesAmount: data.minutesAmountInput,
+    };
+
+    setCycles((state) => [...state, newCycle]);
+    setActiveCycleId(newCycle.id);
+    reset();
+  }
+
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  console.log("ACTIVE", activeCycle);
   return (
     <HomeContainer>
-      <form action="">
+      <form action="" onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput type="text" list="task-suggestion" id="task" placeholder="Dê um nome para o seu projeto" />
-
+          <TaskInput
+            type="text"
+            list="task-suggestion"
+            id="task"
+            placeholder="Dê um nome para o seu projeto"
+            {...register("task")}
+          />
           <datalist id="task-suggestion">
             <option value="Projeto 1" />
             <option value="Projeto 2" />
@@ -24,8 +85,16 @@ export function Home() {
           </datalist>
 
           <label htmlFor="minutesAmount">durantes</label>
-          <MinutesAmountInput step={5} min={5} max={60} type="number" id="minutesAmount" placeholder="00" />
-
+          <MinutesAmountInput
+            step={5}
+            min={5}
+            max={60}
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            {...register("minutesAmountInput", { valueAsNumber: true })}
+          />
+          {/* QUANDO FOR MEXER COM NUMERO NO REACT HOOK FORM PASSAR A PROPRIEDADE VALUESASNUMBER PARA QUE O NUMERO ENVIADO ENTRE "" SEJA PASSADO SEM AS ASPAS CONSERVANDO A TIPAGEM DE NUMBER, EVITANDO ASSIM O ERRO NO ZOD */}
           <span>minutos.</span>
         </FormContainer>
         <CountdownContainer>
@@ -36,7 +105,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton type="submit">
+        <StartCountdownButton type="submit" disabled={!watch("task")}>
           <Play size={24} />
           Começar
         </StartCountdownButton>
