e9f2b0d28698bd4bd3d7f5fe79f3bd94deb9841c
Albert Allan <albertallanramos@gmail.com>
2024-06-15 17:25:03 -0300

    Funcionalidades da aplicação - Interromper ciclo

--- ignite-timer/src/pages/Home/index.tsx
+++ ignite-timer/src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Play } from "phosphor-react";
-import { useState } from "react";
+import { differenceInSeconds } from "date-fns";
+import { Play, Stop } from "phosphor-react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import {
@@ -10,6 +11,7 @@ import {
   MinutesAmountInput,
   Separator,
   StartCountdownButton,
+  StopCountdownButton,
   TaskInput,
 } from "./style";
 
@@ -31,47 +33,91 @@ interface Cycle {
   id: string;
   task: string;
   minutesAmount: number;
+  startDate: Date;
+  interruptedDate?: Date;
 }
 
 export function Home() {
   const [cycles, setCycles] = useState<Cycle[]>([]);
   const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
-  const {
-    register,
-    handleSubmit,
-    watch,
-    reset,
-    formState: { errors },
-  } = useForm<NewCycleFormType>({
+  const [amountSecondPassed, setAmountSecondsPassed] = useState(0);
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormType>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
       minutesAmountInput: 0,
       task: "",
     },
   });
-  console.log("ERRO", errors);
+
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondPassed : 0;
+
+  const minutesAmount = Math.floor(currentSeconds / 60);
+  const secondsAmount = currentSeconds % 60;
+
+  const minutes = String(minutesAmount).padStart(2, "0");
+  const seconds = String(secondsAmount).padStart(2, "0");
+  // PADSTART faz que quando se passa um tamanho especifico e o conteudo que ta
+  // sendo entregue não tem tamanho suficiente para todo o tamanho designado, ele complete com 0
+
+  useEffect(() => {
+    let interval: number;
+
+    if (activeCycle) {
+      interval = setInterval(() => {
+        setAmountSecondsPassed(differenceInSeconds(new Date(), activeCycle.startDate));
+      }, 1000);
+    }
+    // TODO RETURN DENTRO DE UM UseEffect é uma função
+    return () => {
+      clearInterval(interval);
+    };
+  }, [activeCycle]);
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `Ignite Timer / ${minutes}:${seconds} `;
+    }
+  }, [activeCycle, minutes, seconds]);
+
   function handleCreateNewCycle(data: NewCycleFormType) {
-    console.log("DATA", data);
     const newCycle: Cycle = {
       id: String(new Date().getTime()),
       task: data.task,
       minutesAmount: data.minutesAmountInput,
+      startDate: new Date(),
     };
 
     setCycles((state) => [...state, newCycle]);
     setActiveCycleId(newCycle.id);
+    setAmountSecondsPassed(0);
     reset();
   }
 
-  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+  function handleInterruptCycle() {
+    setCycles(
+      cycles.map((cycle) => {
+        if (cycle.id === activeCycleId) {
+          return { ...cycle, interruptedDate: new Date() };
+        } else {
+          return cycle;
+        }
+      }),
+    );
+    setActiveCycleId(null);
+  }
 
-  console.log("ACTIVE", activeCycle);
+  console.log("Cycles", cycles);
   return (
     <HomeContainer>
       <form action="" onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
+          {/* Quando se coloca !! ele vai converter o dado para um bolean, se tiver dado dentro é true senao é false */}
           <TaskInput
+            disabled={!!activeCycle}
             type="text"
             list="task-suggestion"
             id="task"
@@ -86,6 +132,7 @@ export function Home() {
 
           <label htmlFor="minutesAmount">durantes</label>
           <MinutesAmountInput
+            disabled={!!activeCycle}
             step={5}
             min={5}
             max={60}
@@ -98,17 +145,24 @@ export function Home() {
           <span>minutos.</span>
         </FormContainer>
         <CountdownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>{seconds[0]}</span>
+          <span>{seconds[1]}</span>
         </CountdownContainer>
-
-        <StartCountdownButton type="submit" disabled={!watch("task")}>
-          <Play size={24} />
-          Começar
-        </StartCountdownButton>
+        {/* Pode-se passar posicionamento de um elemento para um numero igual em um vetor */}
+        {activeCycle ? (
+          <StopCountdownButton type="button" onClick={handleInterruptCycle}>
+            <Stop size={24} />
+            Parar
+          </StopCountdownButton>
+        ) : (
+          <StartCountdownButton type="submit" disabled={!watch("task")}>
+            <Play size={24} />
+            Começar
+          </StartCountdownButton>
+        )}
       </form>
     </HomeContainer>
   );
